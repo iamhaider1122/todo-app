da9dfc1ca478dab3b1b796eb9caf91084c1aca85
joseph581428 <104356881+joseph581428@users.noreply.github.com>
2024-04-25 18:54:16 +0500

    added express validation on user model and added comment on user routes how extract errors from express validator

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //api endpoint to create a new user
-router.post("/createUser", async (req, res) => {
-  const user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+router.post(
+  "/createUser",
+  [
+    body("name").isLength({ min: 5 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }).exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  res.send( user );
-});
+    if (!errors.isEmpty()) {
+      //this will clg object as errors is of type object
+      console.log(typeof errors);
+      //this will console.log errors object that contain errors as key and array of errors as its value
+      console.log(errors);
+
+      //this will extract errors from errors object and will return them in array form
+      console.log(errors.array());
+      //.json({error:error.array()}) will return json data with key of errors and its value will be an error array
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.send(user);
+    } catch (error) {
+      res.status(500).send("internal server error occured.");
+    }
+  }
+);
+
+//
 
 module.exports = router;
