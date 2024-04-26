c3696ca1d1a8ff8ddfc14a8e252070cc311b9125
joseph581428 <104356881+joseph581428@users.noreply.github.com>
2024-04-26 16:18:41 +0500

    added login endpoint with jwt and bcryptjs for password storing

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwtSecret = "haider";
 //api endpoint to create a new user
 router.post(
   "/createUser",
   [
-    body("name").isLength({ min: 5 }),
+    body("name", "Name must be 5 characters long").isLength({ min: 5 }),
     body("email")
       .isEmail()
       .custom(async (value) => {
-        // Pass req as the second argument
         console.log(value, "farooq haider alvi");
         const existingUser = await User.findOne({ email: value });
         if (existingUser) {
@@ -27,19 +28,69 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
+    const salt = bcrypt.genSaltSync(10);
+
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
     try {
       let user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.send(user);
+
+      const data = {
+        data: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+      res.send({ jwtToken: authToken });
     } catch (error) {
       res.status(500).send("internal server error occured.");
     }
   }
 );
 
-//
+//route for login User
+
+router.post(
+  "/loginUser",
+  [body("email").isEmail().exists(), body("password").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let user = await User.findOne({ email: req.body.email });
+
+      if (!user) {
+        return res.status(400).json({ error: "Invalid username or password" });
+      }
+
+      let comparePass = await bcrypt.compare(req.body.password, user.password);
+
+      if (!comparePass) {
+        res.status(400).json({ error: "invalid email or password" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, jwtSecret);
+      res.json({ token });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 module.exports = router;
+
+// e bcrypt.compare() method to compare a plain text password with a hashed password stored in your database,
+//  bcrypt automatically extracts the salt from the hashed password and uses it for comparison.
